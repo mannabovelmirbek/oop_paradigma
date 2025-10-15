1a405c35127ec7ae275fe45cfc828cc4dff71da7
java_20 <mannabovelmirbek0@gmail.com>
2025-10-15 12:57:41 +0600

    commit

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String name;
+    String model;
+    int year;
+    int probeg;
+    String color;
+
+    public static void drive(){
+        System.out.println("Drive");
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args) {
+      Car car = new Car();
+      car.name = "Toyota";
+      car.model = "Camry";
+      car.color = "Black";
+      car.year = 2022;
+      car.probeg = 86000;
+        System.out.println(car.name);
+        System.out.println(car.model);
+        System.out.println(car.color);
+        System.out.println(car.year);
+        System.out.println(car.probeg);
+}
+
+}
